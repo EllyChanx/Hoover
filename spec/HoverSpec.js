c17875f73eb1dd4c18ecfb4babd24e84e3277221
elly <elly.chan.mda@gmail.com>
2019-03-20 17:33:21 +0000

    add #getRoomDimensions and #getInitialCoord with tests

--- spec/HoverSpec.js
+++ spec/HoverSpec.js
@@ -1,5 +1,29 @@
-describe("Hover", function() {
-  var Hover = require('../src/Hoover');
+describe("Hoover", function() {
+  var Hoover = require('../src/Hoover');
 
+  beforeEach(function() {
+    hoover = new Hoover();
+  });
 
+  it("#getRoomDimensions get the room's width and height", function() {
+    hoover.getRoomDimensions();
+    expect(hoover.roomWidth).toEqual(5);
+    expect(hoover.roomHeight).toEqual(5);
+    });
+
+  it("#initialCoord get the hoover's initial X & Y coord.", function() {
+    hoover.getInitialCoord();
+    expect(hoover.initialXCoord).toEqual(1);
+    expect(hoover.initialYCoord).toEqual(2);
+  });
+
+  it("#initialCoord error if initial X is outside of the room", function() {
+    hoover.roomWidth = 0;
+    expect(function(){hoover.getInitialCoord()}).toThrowError("hoover doesn't work outdoor")
+  });
+
+  it("#initialCoord error if initial Y is outside of the room", function() {
+    hoover.roomHeight = 1;
+    expect(function(){hoover.getInitialCoord()}).toThrowError("hoover doesn't work outdoor")
+  });
 });
--- src/Hoover.js
+++ src/Hoover.js
@@ -2,17 +2,32 @@
 
 function Hoover() {
   let fs = require('fs');
-  var inputInstruction = fs.readFileSync('/home/elly/Desktop/Hoover/input.txt').toString().split("\n");
-  console.log(inputInstruction)
+  this.inputInstruction = fs.readFileSync('/home/elly/Desktop/Hoover/input.txt').toString().split("\n");
+  console.log(this.inputInstruction)
 }
 
-Hoover.prototype.getDimensions = function(array) {
-  
+Hoover.prototype.getRoomDimensions = function() {
+  let roomDimensions = this.inputInstruction[0].split(" ")
+  this.roomWidth = Number(roomDimensions[0])
+  this.roomHeight = Number(roomDimensions[1])
+};
+
+Hoover.prototype.getInitialCoord = function() {
+  let initialCoord = this.inputInstruction[1].split(" ")
+  this.initialXCoord = Number(initialCoord[0])
+  this.initialYCoord = Number(initialCoord[1])
+  if (this.initialXCoord > this.roomWidth || this.initialYCoord > this.roomHeight) { 
+    throw new Error ("hoover doesn't work outdoor") 
+  }
 };
 
 Hoover.prototype.outputResult = function(first_argument) {
-  console.log(true)
+  console.log(true);
+  hoover.getRoomDimensions();
+  hoover.getInitialCoord();
 };
+
+module.exports = Hoover;
 
 let hoover  = new Hoover();
 hoover.outputResult();
