7b11d49cd21d5eedf47c6461aae74660e8ff1d0c
elly <elly.chan.mda@gmail.com>
2019-03-22 01:18:47 +0000

    update function names

--- spec/HoverSpec.js
+++ spec/HoverSpec.js
@@ -5,36 +5,36 @@ describe("Hoover", function() {
     hoover = new Hoover();
   });
 
-  it("#getRoomDimensions get the room's width and height", function() {
-    hoover.getRoomDimensions();
+  it("#getRoomSize get the room's width and height", function() {
+    hoover.getRoomSize();
     expect(hoover.roomWidth).toEqual(5);
     expect(hoover.roomHeight).toEqual(5);
     });
 
-  it("#getRoomDimensions get the hoover's initial X & Y coord.", function() {
+  it("#getInitialCoord get the hoover's initial X & Y coord.", function() {
     hoover.getInitialCoord();
     expect(hoover.initialXCoord).toEqual(1);
     expect(hoover.initialYCoord).toEqual(2);
   });
 
-  it("#getRoomDimensions error if initial X is outside of the room", function() {
+  it("#getInitialCoord error if initial X is outside of the room", function() {
     hoover.roomWidth = 0;
     expect(function(){hoover.getInitialCoord()}).toThrowError("hoover doesn't work outdoor")
   });
 
-  it("#getRoomDimensions error if initial Y is outside of the room", function() {
+  it("#getInitialCoord error if initial Y is outside of the room", function() {
     hoover.roomHeight = 0;
     expect(function(){hoover.getInitialCoord()}).toThrow(new Error("hoover doesn't work outdoor"))
   });
 
-  it("#getMoves get array of the instructed movements", function() {
-    hoover.getMoves();
+  it("#getMoveInstr get array of the instructed movements", function() {
+    hoover.getMoveInstr();
     expect(hoover.moves).toEqual([ 'N', 'N', 'E', 'S', 'E', 'E', 'S', 'W', 'N', 'W', 'W' ])
   });
 
   it("#applyMoves turn moves into move coord trace as ary", function() {
     hoover.getInitialCoord();
-    hoover.getMoves();
+    hoover.getMoveInstr();
     hoover.applyMoves()
     expect(hoover.moveTrace).toContain([ '1', '3' ] && [ '1', '4' ]);
   });
@@ -46,7 +46,7 @@ describe("Hoover", function() {
 
   it("#countDirtCleaned count no. of dirt coords match robot trace", function() {
     hoover.getInitialCoord();
-    hoover.getMoves();
+    hoover.getMoveInstr();
     hoover.applyMoves()
     hoover.getDirtCoord();
     hoover.countDirtCleaned();
